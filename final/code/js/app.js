ac7229be9361f56289f021d68fa2de123cb1d52a
Navix101 <MichaelSullivan@Micahels-MacBook-Pro.local>
2016-04-02 14:28:21 -0400

    MS: Stuff

--- final/code/js/app.js
+++ final/code/js/app.js
@@ -1,3 +1,20 @@
     $(document).ready(function() {
         $("#lightgallery").lightGallery(); 
     });
+
+    var hashTagActive = "";
+    $(".scroll").click(function (event) {
+        if(hashTagActive != this.hash) { 
+            event.preventDefault();
+            var dest = 0;
+            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                dest = $(document).height() - $(window).height();
+            } else {
+                dest = $(this.hash).offset().top;
+            }
+            $('html,body').animate({
+                scrollTop: dest
+            }, 2000, 'swing');
+            hashTagActive = this.hash;
+        }
+    });
